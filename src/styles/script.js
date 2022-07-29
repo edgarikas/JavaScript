7e7518bf24f660f7f4a67a803e41e3e0f8815f38
edgarikaas <edgarasx233@gmail.com>
2022-07-29 12:56:38 +0300

    Push all items

---
+++ src/styles/script.js
@@ -0,0 +1,14 @@
+const box = document.querySelectorAll(".box");
+const paragraph = document.querySelectorAll("p");
+const headings = document.querySelectorAll(".box__title");
+const btn = document.querySelectorAll(".btnBuy");
+const body = document.querySelectorAll("body");
+const a = document.querySelector("a");
+
+const grand = document.getElementsByClassName("grand");
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+
+child.addEventListener("click", (event) => {
+  alert(Hello);
+});
