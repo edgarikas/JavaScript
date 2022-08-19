ecc2f3538ba68e7b3aba361f50d08512b2939609
edgarikas <107058308+edgarikas@users.noreply.github.com>
2022-08-19 11:27:22 +0300

    Delete Business-card directory

--- Business-card/dist/index.24a4377f.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("form").addEventListener("submit", (e)=>{
-    e.preventDefault();
-    const cardInputs = new FormData(e.target);
-    console.log(Object.fromEntries(cardInputs));
-});
-
-//# sourceMappingURL=index.24a4377f.js.map
