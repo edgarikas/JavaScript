2fea1f0ca00eef5dc1d7d1278fbf766bf66a78bf
edgarikaas <edgarasx233@gmail.com>
2022-08-22 13:49:45 +0300

    Update

---
+++ Guest-List/src/script.js
@@ -0,0 +1,50 @@
+let guestList = [
+  "Arda George",
+  "Richard Black",
+  "Eesha Lawrence",
+  "Blade Carroll",
+  "Earl Kinney",
+];
+
+function render() {
+  guestsContainerId = "guestsList";
+  if (document.querySelector(`#${guestsContainerId}`)) {
+    document.querySelector(`#${guestsContainerId}`).remove();
+  }
+
+  if (document.querySelector(`#${guestsContainerId}`)) {
+    console.dir(`#${guestsContainerId}`);
+  }
+  const guests = document.createElement("div");
+  document.querySelector("#app").append(guests);
+  guests.id = guestsContainerId;
+  guestList.forEach((e) => {
+    let names = document.createElement("p");
+    names.textContent += e;
+    guests.append(names);
+  });
+}
+render();
+
+document.querySelector(".buttons").addEventListener("click", (b) => {
+  b.preventDefault();
+  let guestInput = document.querySelector("#guest").value;
+
+  if (b.target.id === "addListEnd") {
+    guestList.push(guestInput);
+    render();
+  } else if (b.target.id === "addListStart") {
+    console.log(guestInput);
+    guestList.unshift(guestInput);
+    render();
+  } else if (b.target.id === "removeFirst") {
+    guestList.shift();
+    render();
+  } else if (b.target.id === "removeLast") {
+    guestList.pop();
+    render();
+  } else if (b.target.id === "reverse") {
+    guestList.reverse();
+    render();
+  }
+});
