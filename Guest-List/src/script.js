548dbf6c08dfe0a0faf04c85ba6f479cdff2b27e
edgarikaas <edgarasx233@gmail.com>
2022-08-25 12:26:11 +0300

    Update

--- Guest-List/src/script.js
+++ Guest-List/src/script.js
@@ -1,5 +1,4 @@
-let guestList = [""];
-let i = 0; //temporary
+let guestList = [];
 
 function render() {
   guestsContainerId = "guestsList";
@@ -24,68 +23,62 @@ function render() {
 document.querySelector("form").addEventListener("click", (b) => {
   b.preventDefault();
   let guestInput = document.querySelector("#guest").value;
+  if (b.target.nodeName === "BUTTON") {
+    switch (b.target.id) {
+      case "addListEnd":
+        guestList.push(guestInput);
+        break;
 
-  switch (b.target.id) {
-    case "addListEnd":
-      guestList.push(guestInput);
-      break;
+      case "addListStart":
+        guestList.unshift(guestInput);
+        break;
 
-    case "addListStart":
-      guestList.unshift(guestInput);
-      break;
+      case "removeFirst":
+        guestList.shift();
+        break;
 
-    case "removeFirst":
-      guestList.shift();
-      break;
+      case "removeLast":
+        guestList.pop();
+        break;
 
-    case "removeLast":
-      guestList.pop();
-      break;
+      case "reverse":
+        guestList.reverse();
+        break;
 
-    case "reverse":
-      guestList.reverse();
-      break;
+      case "removeFromTo":
+        let a = document.querySelector("#firstPoint").value;
+        let b = document.querySelector("#secondPoint").value;
+        guestList.splice(a - 1, parseInt(b) - parseInt(a - 1));
+        break;
 
-    case "removeFromTo":
-      let a = document.querySelector("#firstPoint").value;
-      let b = document.querySelector("#secondPoint").value;
-      guestList.splice(a - 1, parseInt(b) - parseInt(a - 1));
-      break;
+      case "addToCertainPoint":
+        let point = document.querySelector("#certainPoint").value;
+        let name = document.querySelector("#certainPointName").value;
+        point = parseInt(point - 1);
+        guestList.splice(point, 0, `${name}`);
+        break;
 
-    case "addToCertainPoint":
-      let point = document.querySelector("#certainPoint").value;
-      let name = document.querySelector("#certainPointName").value;
-      guestList.splice(point, 0);
-      point = parseInt(point - 1);
-      guestList.splice(point, 0, `${name}`);
-      break;
+      case "firstToLast":
+        let firstName = guestList.shift();
+        guestList.push(firstName);
+        break;
 
-    case "firstToLast":
-      let firstName = guestList.shift();
-      guestList.push(firstName);
-      break;
+      case "lastToFirst":
+        let lastName = guestList.pop();
+        guestList.unshift(lastName);
+        break;
 
-    case "lastToFirst":
-      let lastName = guestList.pop();
-      guestList.unshift(lastName);
-      break;
+      case "sortAZ":
+        guestList = guestList.sort();
+        break;
 
-    case "sortAZ":
-      guestList = guestList.sort();
-      break;
+      case "sortZA":
+        guestList = guestList.sort((a, b) => b.localeCompare(a));
+        break;
 
-    case "sortZA":
-      guestList = guestList.sort().reverse();
-      break;
-
-    default:
-      break;
+      default:
+        break;
+    }
+    render();
   }
-
-  if (i == 0) {
-    guestList.shift(); //Temporary
-    i = 1;
-  }
-
-  render();
 });
