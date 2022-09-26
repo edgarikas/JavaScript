23d2e1b7806583728711fab5e87da7ce04ff63bb
edgarikaas <edgarasx233@gmail.com>
2022-09-26 13:22:49 +0300

    Update

--- Trello/src/script.js
+++ Trello/src/script.js
@@ -1,11 +1,5 @@
-const list = document.querySelectorAll(".list");
-const btn = document.querySelector(".add__btn");
-const addBtn = document.querySelector(".add__item-btn");
-const cancelBtn = document.querySelector(".cancel__item-btn");
-const textarea = document.querySelector(".textarea");
-const form = document.querySelector(".form");
 let newBoard = document.querySelector(".button");
-let dragedItem = null;
+let dragedItem = "0";
 
 function addBoard() {
   const boards = document.querySelector(".boards");
@@ -25,12 +19,24 @@ function addBoard() {
 
   const li = document.createElement("div");
   li.classList = "list";
+  const liItem = document.createElement("div");
+  liItem.draggable = "true";
+  liItem.classList = "list__item";
+  liItem.textContent = "";
+
+  const p = document.createElement("p");
+  p.textContent = "Starting card";
+  const delL = document.createElement("button");
+  delL.textContent = "X";
+  delL.classList = "list__delete";
+  liItem.append(p, delL);
 
   const f = document.createElement("div");
   f.classList = "form";
   const textA = document.createElement("textarea");
   textA.classList = "textarea";
   textA.placeholder = "Enter Card Name";
+  textA.textContent = "";
   const buttons = document.createElement("div");
   buttons.classList = "buttons";
 
@@ -38,60 +44,73 @@ function addBoard() {
   add.classList = "add__item-btn";
   add.textContent = "Add Card";
   const cancel = document.createElement("button");
-  cancel.classList = "cancel-btn";
+  cancel.classList = "cancel__item-btn";
   cancel.textContent = "Cancel";
 
-  buttons.append(add.cancel);
+  const addCard = document.createElement("button");
+  addCard.classList = "add__btn";
+  addCard.textContent = "+ Add Card";
 
+  buttons.append(add, cancel);
   f.append(textA, buttons);
-
-  board.append(cHeader, li, f);
-
+  li.append(liItem);
+  board.append(cHeader, li, f, addCard);
   boards.append(board);
+
+  //
+  let value = "";
+  addCard.addEventListener("click", (event) => {
+    event.target.style.display = " none";
+    f.style.display = " block";
+    add.style.display = "none";
+    textA.addEventListener("input", (e) => {
+      value = e.target.value;
+      if (value) {
+        add.style.display = "block";
+      } else {
+        add.style.display = "none";
+      }
+    });
+  });
+
+  cancel.addEventListener("click", () => {
+    clearData(addCard, f, textA);
+  });
+
+  add.addEventListener("click", (e) => {
+    let list = li;
+    const newItem = document.createElement("div");
+    newItem.classList = "list__item";
+    newItem.draggable = "true";
+    //newItem.textContent = value;
+
+    const p = document.createElement("p");
+    p.textContent = value;
+    const delB = document.createElement("button");
+    delB.textContent = "X";
+    delB.classList = "list__delete";
+
+    newItem.append(p, delB);
+
+    newItem.addEventListener("mouseover", () => {
+      dragAndDrop();
+    });
+    list.append(newItem);
+    clearData(addCard, f, textA);
+  });
+
+  //
 
   changeTilte();
   dragAndDrop();
   removeCard();
 }
 
-function addTask() {
-  let value;
-  btn.addEventListener("click", () => {
-    form.style.display = "block";
-    btn.style.display = "none";
-    addBtn.style.display = "none";
-    textarea.addEventListener("input", (e) => {
-      value = e.target.value;
-
-      if (value) {
-        addBtn.style.display = "block";
-      } else {
-        addBtn.style.display = "none";
-      }
-    });
-  });
-
-  cancelBtn.addEventListener("click", () => {
-    clearData();
-  });
-
-  addBtn.addEventListener("click", () => {
-    const newItem = document.createElement("div");
-    newItem.classList = "list__item";
-    newItem.draggable = "true";
-    newItem.textContent = value;
-    list[0].append(newItem);
-
-    clearData();
-    dragAndDrop();
-  });
-}
-
-function clearData() {
-  textarea.value = "";
+function clearData(addCard, f, textA) {
+  addCard.style.display = "block";
+  f.style.display = "none";
+  textA.value = "";
   value = "";
-  form.style.display = "none";
-  btn.style.display = "flex";
 }
 
 function changeTilte() {
@@ -117,49 +136,59 @@ function dragAndDrop() {
   for (let i = 0; i < listItems.length; i++) {
     const item = listItems[i];
 
-    item.addEventListener(
-      "dragstart",
-      () => {
-        dragedItem = item;
+    item.addEventListener("dragstart", () => {
+      dragedItem = item;
 
-        setTimeout(() => (item.style.display = " none"));
-      },
-      0
-    );
+      console.log(`Item -> `, item);
+      console.log("Draged Item -> ", dragedItem);
+
+      setTimeout(() => {
+        item.style.display = " none";
+      }, 0);
+    });
 
     item.addEventListener("dragend", () => {
       setTimeout(() => {
-        item.style.display = "block";
+        item.style.display = "flex";
         dragedItem = null;
       }, 0);
     });
 
-    item.addEventListener("dblclick", () => {
-      item.remove();
+    item.addEventListener("click", (e) => {
+      //remove
+      console.log(e.target.className);
+      if (e.target.className === "list__delete") {
+        item.remove();
+      }
     });
 
     for (let j = 0; j < lists.length; j++) {
       const list = lists[j];
-      list.addEventListener("dragover", (e) => e.preventDefault());
+      list.addEventListener("dragover", (e) => {
+        e.preventDefault();
+      });
       list.addEventListener("dragenter", function (e) {
         e.preventDefault();
         this.style.backgroundColor = "rgba(0,0,0, .3)";
+        this.style.paddingBottom = "2.5rem";
       });
       list.addEventListener("dragleave", function (e) {
         e.preventDefault();
         this.style.backgroundColor = "rgba(0,0,0, 0)";
+        this.style.paddingBottom = "0";
       });
       list.addEventListener("drop", function (e) {
         this.style.backgroundColor = "rgba(0,0,0, 0)";
+        this.style.paddingBottom = "0";
+
         this.append(dragedItem);
       });
     }
   }
 }
 
-addTask();
-changeTilte();
-removeCard();
-dragAndDrop();
-
 newBoard.addEventListener("click", addBoard);
+
+window.addEventListener("DOMContentLoaded", () => {
+  dragAndDrop();
+});
